c026962573c8de1fb1b788163be753933cbb9797
59013-27-13 <59013-27-13@exchange.ad.afpanet>
2022-05-25 16:02:47 +0200

    exercice jS avec Udemy

---
+++ .vscode/basics/functionAnatomy1.js
@@ -0,0 +1,9 @@
+// déclaration des fonctions
+/*commentaire multiligne
+JavaScript*/
+// console.log ('hello !') pour afficher 'hello' utiliser guillemet du 4 ou 3
+// pour afficher la constante `${person1}` dans le log utiliser les guillemets du 7``
+
+const person1 = 'Mark'
+console.log(`Hello, ${person1}!`)
+
